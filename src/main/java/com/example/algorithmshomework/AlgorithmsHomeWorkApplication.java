e959d0ea6261a32bcd4bf5a85e8415f88738d9f5
Anastasiyu <asy.safiulina@mail.ru>
2022-10-22 17:41:50 +0300

    Initial commit

---
+++ src/main/java/com/example/algorithmshomework/AlgorithmsHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithmshomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsHomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algorithmshomework/AlgorithmsHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithmshomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
